b499b53d3d509aeb767c525aed8348e16bdfa0be
yoh <yokeus2000@gmail.com>
2025-09-20 21:04:36 +0900

    🗃️ db: 匿名認証用のisAnonymousフィールドを追加

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,9 @@
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as authSchema from "./schemas/auth"; // import your schema here
+import * as authSchema from "@/db/schemas/auth";
 
 config({ path: ".env" });
 
-const client = postgres(process.env.DATABASE_URL!);
+const client = postgres(process.env.DATABASE_URL!, { prepare: false });
 export const db = drizzle({ client, schema: { ...authSchema } });
--- src/db/schemas/auth.ts
+++ src/db/schemas/auth.ts
@@ -6,6 +6,7 @@ export const users = pgTable("users", {
   email: text("email").notNull().unique(),
   emailVerified: boolean("email_verified").default(false).notNull(),
   image: text("image"),
+  isAnonymous: boolean("is_anonymous").default(false).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at")
     .defaultNow()
